d3f9eaf40160aa7ce85e567f267c85ec62e8fb3e
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-08-12 20:58:31 -0400

    stopping point

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-//API for selecting cat breed: https://api.thecatapi.com/v1/breeds
+// /api/tags => Will return all tags. Use to poulate dropdown.
 
+// /cat/:tag/says/:text?color=:color=> I think this will return a random cat with a :tag, :text, and :color selected. 
+
+const formData = {
+    //form.helloKitty
+  };
+  
+  const configurationObject = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  fetch("https://cataas.com/cat/:tag/says/:text?color=:color", configurationObject)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      console.log(object);
+    }); 
