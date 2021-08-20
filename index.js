ca98639df49a8a64231ab74609181589c9c33006
Bridgett Wheeler <bridgettwheeler@gmail.com>
2021-08-20 12:00:49 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 
-//Notes from instructor: You need an event listener on submit. listener for the form inside that. Take out the values from the form. Then fetch
-  
-//1. event listener for submit - stop the default and create var for input value. then fetch for those key/values. once the data is returned display it.
-// use this three times to get input values: const newEmoji = evt.target['emoji'].value
-//create a div for an image that is empty then change the source 
-//need two more event listeners
-const kittyForm = document.querySelector(".kittyForm")
+const kittyForm = document.querySelector(".kittyForm");
 
 kittyForm.addEventListener('submit', (evt) => {
     evt.preventDefault()
@@ -13,15 +7,33 @@ kittyForm.addEventListener('submit', (evt) => {
     const catTag = evt.target['tag'].value
     const catText = evt.target['text'].value
     const catColor = evt.target['color'].value
-
+console.log(catColor)
 
     fetch(`https://cataas.com/cat/${catTag}/says/${catText}?color=${catColor}`)
     .then(function(object) {
-        const catPic = document.querySelector(".catPic")
-        catPic.img.src = object.url
+        document.querySelector("#catPic").src = object.url
+        console.log(object)
     });  
     
 
+})
+let imageArea = document.querySelector("#catPic")
+imageArea.addEventListener('mouseover', (evt) => {
+    evt.target.style.cursor = "pointer";
+    
+})
+
+imageArea.addEventListener('click', (evt) => {
+    // var audioElement = new Audio('Meow.wav')
+    // audioElement.play()
+
+    // var audio = document.createElement('audio');
+    // audio.src = 'Meow.wav'
+    // audio.play();
+
+    var myAudio1 = document.getElementById("audio1");
+    myAudio1.play()
+    
 })
 
 
